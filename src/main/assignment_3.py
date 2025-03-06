ace2c6c742839d7abc70036f78da7c6cd63a7eed
GageLappin119 <gagelappin119@gmail.com>
2025-03-06 11:35:49 -0500

    Update assignment_3.py

--- src/main/assignment_3.py
+++ src/main/assignment_3.py
@@ -17,10 +17,10 @@ def Runge_Kutta(range_start, range_end, iterations, initial_point_y, f, h):
     values = [initial_point_y]  # Store y values dynamically
 
     for i in range(iterations):
-        k_1 = h * f(h * i, values[i])
-        k_2 = h * f((h * i) + h/2, values[i] + k_1/2)
-        k_3 = h * f((h * i) + h/2, values[i] + k_2/2)
-        k_4 = h * f((h * i) + h, values[i] + k_3)
+        k_1 = h * f(range_start + (h * i), values[i])
+        k_2 = h * f(range_start + (h * i) + h/2, values[i] + k_1/2)
+        k_3 = h * f(range_start + (h * i) + h/2, values[i] + k_2/2)
+        k_4 = h * f(range_start + (h * i) + h, values[i] + k_3)
         values.append(values[i] + (1/6) * (k_1 + 2*k_2 + 2 * k_3 + k_4))
 
     return values
